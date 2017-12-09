abdd1e3a8bbff3de5e5b247dbcf0c1fd9ee9f40c
= <lorin_ag@163.com>
2017-12-10 00:59:24 +0800

    添加了redis的java客户端Jedis在测试类中 并且在pom文件中引入了两个相关jar包

--- src/main/java/com/yuan/first/GirlController.java
+++ src/main/java/com/yuan/first/GirlController.java
@@ -3,6 +3,7 @@ package com.yuan.first;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -14,7 +15,6 @@ public class GirlController {
 
     @GetMapping(value = "/list")
     public List<Girl> girlList(){
-
         System.out.println("llllllllllllllllllllllllllllllllll");
         return rep.findAll();
     }
--- src/test/java/com/yuan/first/FirstApplicationTests.java
+++ src/test/java/com/yuan/first/FirstApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,11 @@ public class FirstApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Jedis jedis = new Jedis("39.106.127.238",6379);
+		jedis.set("11", "123");
+		String value = jedis.get("11");
+		System.out.println(value);
+		jedis.close();
 	}
 
 }
